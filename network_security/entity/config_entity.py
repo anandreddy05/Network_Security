b97c63852c5bc8b0a8a0104ffc647eda97c0ea87
Anand <anandreddy.s3215@gmail.com>
2025-06-23 23:55:26 +0530

    update conifig_entity

--- network_security/entity/config_entity.py
+++ network_security/entity/config_entity.py
@@ -9,7 +9,7 @@ class TrainingPipelineConfig:
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_DIR
-        self.artifact_dir = os.path.join(training_pipeline.ARTIFACT_DIR, timestamp)  # ✅ Fix here
+        self.artifact_dir = os.path.join(training_pipeline.ARTIFACT_DIR, timestamp) 
         self.timestamp: str = timestamp
 
 
